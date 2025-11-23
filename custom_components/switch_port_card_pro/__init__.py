66949f48cd327aee0d4294347ce63619a65a64b7
partach <partach@gmail.com>
2025-11-23 12:07:36 +0100

    Update __init__.py

--- custom_components/switch_port_card_pro/__init__.py
+++ custom_components/switch_port_card_pro/__init__.py
@@ -1,1 +1,8 @@
-
+   from homeassistant.config_entries import ConfigEntry
+   from homeassistant.core import HomeAssistant
+   
+   async def async_setup(hass: HomeAssistant, config: dict) -> bool:
+       return True
+   
+   async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+       return True
