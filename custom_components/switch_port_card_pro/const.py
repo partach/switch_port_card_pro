1fe06829879e19f43e1776f32f2812f7f8987219
partach <partach@gmail.com>
2025-11-25 20:49:22 +0100

    Update const.py

--- custom_components/switch_port_card_pro/const.py
+++ custom_components/switch_port_card_pro/const.py
@@ -6,9 +6,11 @@ CONF_PORTS = "ports"
 CONF_INCLUDE_VLANS = "include_vlans"
 CONF_BASE_OIDS = "base_oids"
 
-DEFAULT_PORTS = list(range(1, 29))  # XGS1935-28
+DEFAULT_PORTS = list(range(1, 29))
 DEFAULT_BASE_OIDS = {
     "rx": "1.3.6.1.2.1.2.2.1.10",
     "tx": "1.3.6.1.2.1.2.2.1.16",
-    "status": "1.3.6.1.2.1.2.2.1.8"
+    "status": "1.3.6.1.2.1.2.2.1.8",
+    "speed": "1.3.6.1.2.1.2.2.1.5",
+    "name": "1.3.6.1.2.1.31.1.1.1.18"
 }
