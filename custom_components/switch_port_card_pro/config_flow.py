537119989dd139b0a9a58d938410c45a5cc81312
partach <partach@gmail.com>
2025-12-01 18:42:54 +0100

    Update config_flow.py

--- custom_components/switch_port_card_pro/config_flow.py
+++ custom_components/switch_port_card_pro/config_flow.py
@@ -124,7 +124,7 @@ class SwitchPortCardProOptionsFlow(config_entries.OptionsFlow):
 
     def __init__(self, config_entry: ConfigEntry) -> None:
         """Initialize."""
-        self.config_entry = config_entry
+    #    self.config_entry = config_entry # depricated in HA 2025.12
 
     async def async_step_init(
         self, user_input: dict[str, Any] | None = None
