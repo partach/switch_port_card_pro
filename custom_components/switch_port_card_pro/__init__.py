686a5636dc4e4bb36d9b9b03d92e406e4a58c696
partach <partach@gmail.com>
2025-12-21 16:02:15 +0100

    Update __init__.py

--- custom_components/switch_port_card_pro/__init__.py
+++ custom_components/switch_port_card_pro/__init__.py
@@ -175,7 +175,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     # Background first refresh
-    hass.loop.create_task(coordinator.async_refresh())
+    await coordinator.async_config_entry_first_refresh()
 
     entry.async_on_unload(entry.add_update_listener(async_options_updated))
 
